40fe454048e40fc1f27e7d4ec55a00f993368924
shashwathkpy <shashwathkpy@gmail.com>
2023-09-17 01:37:45 -0700

    form validation

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function Book(entryID, title, author, pages, read)
+function Book(title, author, pages, read)
 {
-    this.entryID = entryID;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -19,130 +18,177 @@ function Book(entryID, title, author, pages, read)
     }
 }
 
-const TKAMB = new Book("To Kill a Mockingbird", "Harper Lee", 281, true);
-const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-const giver = new Book("The Giver", "Lois Lowry", 240, true);
-let book = 0;
+// const TKAMB = new Book("To Kill a Mockingbird", "Harper Lee", 281, true);
+// const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+// const giver = new Book("The Giver", "Lois Lowry", 240, true);
 
-const addBtn = document.createElement('button');
-addBtn.textContent = 'Add';
-
-const inputPlaceHolder = document.querySelector('#formPlaceholder');
-
-let inputs;
-let titleLabel;
-let titleInput;
-let authorLabel;
-let authorInput;
-let pagesLabel;
-let pagesInput;
-let readLabel;
-let readInput;
+const form = document.querySelector('#form');
 
 const addBookBtn = document.querySelector('#addBookBtn');
-
-addBookBtn.addEventListener('click', function (e) {
-
-    addBookBtn.style.visibility = 'hidden';
-    inputs = document.createElement('div');
-
-    titleLabel = document.createElement('label');
-    titleLabel.textContent = "Title:";
-    titleInput = document.createElement('input');
-    titleInput.setAttribute('type', 'text');
-
-    authorLabel = document.createElement('label');
-    authorLabel.textContent = "Author:";
-    authorInput = document.createElement('input');
-    authorInput.setAttribute('type', 'text');
-
-    pagesLabel = document.createElement('label');
-    pagesLabel.textContent = "# of Pages:";
-    pagesInput = document.createElement('input');
-    pagesInput.setAttribute('type', 'number');
-
-    readLabel = document.createElement('label');
-    readLabel.textContent = "Have you read this book?";
-    readInput = document.createElement('input');
-    readInput.setAttribute('type', 'checkbox');
-
-    inputs.appendChild(titleLabel);
-    inputs.appendChild(document.createElement('br'));
-    inputs.appendChild(titleInput);
-    inputs.appendChild(document.createElement('br'));
-    inputs.appendChild(authorLabel);
-    inputs.appendChild(document.createElement('br'));
-    inputs.appendChild(authorInput);
-    inputs.appendChild(document.createElement('br'));
-    inputs.appendChild(pagesLabel);
-    inputs.appendChild(document.createElement('br'));
-    inputs.appendChild(pagesInput);
-    inputs.appendChild(document.createElement('br'));
-
-    inputs.appendChild(readInput);
-    inputs.appendChild(readLabel);
-    inputs.appendChild(document.createElement('br'));
-    inputs.appendChild(addBtn);
-    inputPlaceHolder.appendChild(inputs);
-});
-
-entryCount = 0;
-addBtn.addEventListener('click', function (e) {
-    entryCount += 1;
-    book = new Book(entryCount, titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
-    addBookToLibrary(book);
-    inputs.remove();
-    addBookBtn.style.visibility = 'visible';
-});
-
 let library = [];
-const table = document.querySelector("#libraryList");
-
-function addBookToLibrary(book)
+addBookBtn.onclick = function ()
 {
-    // library.push(book);
+    addBook();
+}
 
-    // const index = library.indexOf(book.entryID);
-    // library.splice();
-    const entry = document.createElement('tr');
-    entry.setAttribute('id', entryCount);
+function addBook()
+{
+    addBookBtn.style.visibility = 'hidden';
+    form.style.visibility = 'visible';
 
-    const title = document.createElement('td');
-    title.textContent = book.title;
+    const titleInput = document.createElement('input');
+    titleInput.type = 'text';
+    titleInput.id = 'titleInput';
+    titleInput.required = true;
 
-    const author = document.createElement('td');
-    author.textContent = book.author;
+    const titleLabel = document.createElement('label');
+    titleLabel.textContent = "Title:";
+    titleLabel.htmlFor = titleInput.id;
 
-    const pages = document.createElement('td');
-    pages.textContent = book.pages;
+    const authorInput = document.createElement('input');
+    authorInput.type = 'text';
+    authorInput.id = 'authorInput';
+    authorInput.required = true;
 
-    const readButtonSlot = document.createElement('td');
-    const readBtn = document.createElement('button');
+    const authorLabel = document.createElement('label');
+    authorLabel.textContent = "Author:";
+    authorLabel.htmlFor = authorInput.id;
 
-    if(book.read)
-        readBtn.textContent = "Read";
-    else
-        readBtn.textContent = "Not Read";
+    const pagesInput = document.createElement('input');
+    pagesInput.type = 'number';
+    pagesInput.id = 'pagesInput';
+    pagesInput.required = true;
 
-    readBtn.setAttribute('id', entryCount);
-    readBtn.setAttribute('class', 'read');
-    readBtn.setAttribute('onclick', 'toggleRead(this.id)');
+    const pagesLabel = document.createElement('label');
+    pagesLabel.textContent = "# of Pages:";
+    pagesLabel.htmlFor = pagesLabel.id;
 
-    const removeButtonSlot = document.createElement('td');
-    const removeBtn = document.createElement('button');
-    removeBtn.textContent = '✓';
-    removeBtn.setAttribute('id', entryCount);
-    removeBtn.setAttribute('onclick', 'removeEntry(this.id)');
+    const readDiv = document.createElement('div');
+    readDiv.id = 'readDiv';
+    const readInput = document.createElement('input');
+    readInput.type = 'checkbox';
+    readInput.id = 'readInput';
 
-    entry.appendChild(title);
-    entry.appendChild(author);
-    entry.appendChild(pages);
-    readButtonSlot.appendChild(readBtn);
-    entry.appendChild(readButtonSlot);
-    removeButtonSlot.appendChild(removeBtn);
-    entry.appendChild(removeButtonSlot);
+    const readLabel = document.createElement('label');
+    readLabel.textContent = "Have you read this book?";
+    readLabel.htmlFor = readInput.id;
 
-    table.appendChild(entry);
+    readDiv.appendChild(readLabel);
+    readDiv.appendChild(readInput);
+
+    const submit = document.createElement('input');
+    submit.type = 'submit';
+
+    form.appendChild(titleLabel);
+    form.appendChild(titleInput);
+    form.appendChild(authorLabel);
+    form.appendChild(authorInput);
+    form.appendChild(pagesLabel);
+    form.appendChild(pagesInput);
+    form.appendChild(readDiv);
+    form.appendChild(submit);
+    form.onsubmit = function(e)
+    { 
+        e.preventDefault();
+        addBookToLibrary()
+    };
+}
+
+function addBookToLibrary()
+{
+    const titleInput = document.querySelector('#titleInput');
+    const authorInput = document.querySelector('#authorInput');
+    const pagesInput = document.querySelector('#pagesInput');
+    const readInput = document.querySelector('#readInput');
+
+    if(titleInput.value && authorInput.value && pagesInput.value)
+    {
+        const book = new Book(
+            titleInput.value,
+            authorInput.value,
+            pagesInput.value,
+            readInput.value
+        );
+        updateLibrary(book);
+        form.innerHTML = "";
+        form.style.visibility = 'hidden';
+        addBookBtn.style.visibility = 'visible';
+    }
+}
+
+const table = document.querySelector("#libraryList");
+function createLibraryTable()
+{
+    const titleHeader = document.createElement('th');
+    titleHeader.textContent = 'Title';
+
+    const authorHeader = document.createElement('th');
+    authorHeader.textContent = 'Author';
+
+    const pagesHeader = document.createElement('th');
+    pagesHeader.textContent = 'Pages';
+
+    const readHeader = document.createElement('th');
+    readHeader.textContent = 'Read';
+
+    const removeHeader = document.createElement('th');
+    removeHeader.textContent = 'Remove';
+
+    table.appendChild(titleHeader);
+    table.appendChild(authorHeader);
+    table.appendChild(pagesHeader);
+    table.appendChild(readHeader);
+    table.appendChild(removeHeader);
+}
+
+function updateLibrary(book)
+{
+    library.push(book);
+    console.log(library);
+    table.innerHTML = "";
+    createLibraryTable();
+    for(let i = 0; i < library.length; i++)
+    {
+        const entry = document.createElement('tr');
+        entry.setAttribute('id', i);
+    
+        const title = document.createElement('td');
+        title.textContent = library[i].title;
+    
+        const author = document.createElement('td');
+        author.textContent = library[i].author;
+    
+        const pages = document.createElement('td');
+        pages.textContent = library[i].pages;
+    
+        const readButtonSlot = document.createElement('td');
+        const readBtn = document.createElement('button');
+    
+        if(library[i].read)
+            readBtn.textContent = "Read";
+        else
+            readBtn.textContent = "Not Read";
+    
+        readBtn.setAttribute('id', i);
+        readBtn.setAttribute('class', 'read');
+        readBtn.setAttribute('onclick', 'toggleRead(this.id)');
+    
+        const removeButtonSlot = document.createElement('td');
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = '✓';
+        removeBtn.setAttribute('id', i);
+        removeBtn.setAttribute('onclick', 'removeEntry(this.id)');
+    
+        entry.appendChild(title);
+        entry.appendChild(author);
+        entry.appendChild(pages);
+        readButtonSlot.appendChild(readBtn);
+        entry.appendChild(readButtonSlot);
+        removeButtonSlot.appendChild(removeBtn);
+        entry.appendChild(removeButtonSlot);
+    
+        table.appendChild(entry);
+    }
 }
 
 function removeEntry(clickedID)
